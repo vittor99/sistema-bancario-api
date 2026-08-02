3499f2b577466f77feb7a6e1f2442e15dff97375
vittor99 <kawevittor367@gmail.com>
2026-08-01 23:54:31 -0300

    Inicializando projeto Spring Boot V2

---
+++ sistema-bancario-api/src/main/java/com/vittor/sistema_bancario_api/SistemaBancarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.vittor.sistema_bancario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBancarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBancarioApiApplication.class, args);
+	}
+
+}
---
+++ sistema-bancario-api/src/test/java/com/vittor/sistema_bancario_api/SistemaBancarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittor.sistema_bancario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
