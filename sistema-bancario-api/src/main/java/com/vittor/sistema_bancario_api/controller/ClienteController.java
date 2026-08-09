f8be1da35518188948ed57acdf63549acdb0af30
vittor99 <kawevittor367@gmail.com>
2026-08-09 16:24:20 -0300

    criei endpoint para cadastro de clientes e configurei persistência com MySQL

--- sistema-bancario-api/src/main/java/com/vittor/sistema_bancario_api/controller/ClienteController.java
+++ sistema-bancario-api/src/main/java/com/vittor/sistema_bancario_api/controller/ClienteController.java
@@ -1,5 +1,23 @@
 package com.vittor.sistema_bancario_api.controller;
 
+import com.vittor.sistema_bancario_api.entity.Cliente;
+import com.vittor.sistema_bancario_api.service.ClienteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClienteController {
 
+    private final ClienteService clienteService;
+
+
+        ClienteController (ClienteService clienteService){
+            this.clienteService = clienteService;
+        }
+
+    @PostMapping("/clientes")
+    public Cliente salvar(@RequestBody Cliente cliente) {
+        return clienteService.salvar(cliente);
+    }
 }
