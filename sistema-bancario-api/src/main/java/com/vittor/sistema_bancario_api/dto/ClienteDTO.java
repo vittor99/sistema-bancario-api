d0364cebeeaf5f44cde9ac8d631e02f51f46011f
vittor99 <kawevittor367@gmail.com>
2026-08-28 20:53:36 -0300

    adiciona validacao de telefone

--- sistema-bancario-api/src/main/java/com/vittor/sistema_bancario_api/dto/ClienteDTO.java
+++ sistema-bancario-api/src/main/java/com/vittor/sistema_bancario_api/dto/ClienteDTO.java
@@ -8,15 +8,23 @@ import jakarta.validation.constraints.Size;
 
 public class ClienteDTO {
     @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
     private String nome;
     @NotBlank(message = "O CPF é obrigatório")
-    @Pattern(regexp = "\\d{11}", message = "O CPF deve conter 11 números") //para receber apenas numeros
+    @Pattern(
+            regexp = "\\d{11}",
+            message = "O CPF deve conter 11 números"
+    ) //para receber apenas numeros
     @ValidCpf
     private String cpf;
     @NotBlank(message = "O email é obrigatório")
     @Email(message ="O email deve ser válido")
     private String email;
     @NotBlank(message = "O telefone é obrigatório")
+    @Pattern(
+            regexp = "\\d{10,11}",
+            message = "O telefone deve conter 10 ou 11 números"
+    )
     private String telefone;
 
 
